4fc35c1885466b20a5fbbfd675336c1ece00e70c
Scott Perry <github@numist.net>
2013-06-25 22:53:52 -0400

    Always be deleting/refactoring code.

--- Switch/NNWindow+NNWindowFiltering.m
+++ Switch/NNWindow+NNWindowFiltering.m
@@ -25,35 +25,21 @@
 {
     return [array filteredArrayUsingPredicate:[NSPredicate predicateWithBlock:^BOOL(id evaluatedObject, NSDictionary *bindings) {
         NNWindow *window = evaluatedObject;
-
-        // Inexpensive/generic checks first.
-        if (![window isValidWindow]) {
-            return NO;
-        }
+        
+        // Only match windows at kCGNormalWindowLevel
+        BailUnless([[window.windowDescription objectForKey:(__bridge NSString *)kCGWindowLayer] longValue] == kCGNormalWindowLevel, NO);
         
         // Issue #2: Tweetbot composites multiple windows to make up its main window.
-        if ([window.application.name isEqualToString:@"Tweetbot"] && ![window.name length]) {
+        BailUnless(![window.application.name isEqualToString:@"Tweetbot"] || [window.name length], NO);
+        
+        // It's useful to know if/when/why a window is not accessible by Switch. For now, these windows/applications are not supported.
+        if ([[window.windowDescription objectForKey:(__bridge NSString *)kCGWindowSharingState] longValue] == kCGWindowSharingNone) {
+            DebugBreak();
             return NO;
         }
         
         return YES;
     }]];
-}
-
-- (BOOL)isValidWindow;
-{
-    // Only match windows at kCGNormalWindowLevel
-    if ([[self.windowDescription objectForKey:(__bridge NSString *)kCGWindowLayer] longValue] != kCGNormalWindowLevel) {
-        return NO;
-    }
-
-    // It's useful to know if/when/why a window is not accessible by Switch!
-    if ([[self.windowDescription objectForKey:(__bridge NSString *)kCGWindowSharingState] longValue] == kCGWindowSharingNone) {
-        DebugBreak();
-        return NO;
-    }
-
-    return YES;
 }
 
 @end
